fa6b768637d0677cfc9a3a0b12a1f67deb92e151
raoulx24 <raoulx24@yahoo.com>
2025-12-06 17:20:05 +0200

    dotnet 10

--- src/TrivyOperator.Dashboard/Program.cs
+++ src/TrivyOperator.Dashboard/Program.cs
@@ -47,7 +47,7 @@ builder.Services.Configure<ForwardedHeadersOptions>(options =>
         options.ForwardedHeaders = ForwardedHeaders.XForwardedFor |
                                    ForwardedHeaders.XForwardedProto |
                                    ForwardedHeaders.XForwardedHost;
-        options.KnownNetworks.Clear();
+        options.KnownIPNetworks.Clear();
         options.KnownProxies.Clear();
     }
 );
