3407e3a6ebf4fb93bcd2360c7a037e1e5d1523d9
raoulx24 <raoulx24@yahoo.com>
2026-01-12 23:35:25 +0200

    first step for file support

---
+++ src/TrivyOperator.Dashboard/Domain/Trivy/Services/FileFolderNameFactory.cs
@@ -0,0 +1,28 @@
+﻿using TrivyOperator.Dashboard.Domain.Trivy.ClusterInfraAssessmentReport;
+using TrivyOperator.Dashboard.Domain.Trivy.ClusterSbomReport;
+using TrivyOperator.Dashboard.Domain.Trivy.ClusterVulnerabilityReport;
+using TrivyOperator.Dashboard.Domain.Trivy.ExposedSecretReport;
+using TrivyOperator.Dashboard.Domain.Trivy.SbomReport;
+using TrivyOperator.Dashboard.Domain.Trivy.VulnerabilityReport;
+
+namespace TrivyOperator.Dashboard.Domain.Trivy.Services;
+
+public class FileFolderNameFactory
+{
+    public string Get<TKubernetesObject>()
+        => typeof(TKubernetesObject) switch
+        {
+            //{ } t when t == typeof(ClusterComplianceReportCr) => "",
+            { } t when t == typeof(ClusterInfraAssessmentReportCr) => "cluster_infra_assessment_reports",
+            //{ } t when t == typeof(ClusterRbacAssessmentReportCr) => "",
+            { } t when t == typeof(ClusterSbomReportCr) => "cluster_sbom_reports",
+            { } t when t == typeof(ClusterVulnerabilityReportCr) => "cluster_vulnerability_reports",
+            //{ } t when t == typeof(ConfigAuditReportCr) => "",
+            { } t when t == typeof(ExposedSecretReportCr) => "secret_reports",
+            //{ } t when t == typeof(InfraAssessmentReportCr) => "",
+            //{ } t when t == typeof(RbacAssessmentReportCr) => "",
+            { } t when t == typeof(SbomReportCr) => "sbom_reports",
+            { } t when t == typeof(VulnerabilityReportCr) => "vulnerability_reports",
+            _ => throw new InvalidOperationException($"Unsupported Kubernetes object type - {typeof(TKubernetesObject)}"),
+        };
+}
