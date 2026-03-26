8df39a0e25fd684424a06f014bcd9d8c70034642
raoulx24 <raoulx24@yahoo.com>
2026-03-27 00:01:46 +0200

    more toying with severity count dif display

--- src/TrivyOperator.Dashboard/ClientApp/src/app/pipes/counter-icon.pipe.ts
+++ src/TrivyOperator.Dashboard/ClientApp/src/app/pipes/counter-icon.pipe.ts
@@ -11,9 +11,13 @@ export class CounterIconPipe implements PipeTransform {
     }
     if (data > 6) {
       return 'pi pi-arrow-up';
+      // return 'pi pi-plus-circle';
+      // return 'pi pi-arrow-circle-up';
     }
     if (data < 6) {
       return 'pi pi-arrow-down';
+      // return 'pi pi-minus-circle';
+      // return 'pi pi-arrow-circle-down';
     }
 
     return "";
