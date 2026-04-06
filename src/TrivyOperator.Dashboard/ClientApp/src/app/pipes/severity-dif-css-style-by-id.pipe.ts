4fd213fa8ada78db9cfbe76bed8efc42d314dfae
raoulx24 <raoulx24@yahoo.com>
2026-04-06 23:25:43 +0300

    front - small fixes

--- src/TrivyOperator.Dashboard/ClientApp/src/app/pipes/severity-dif-css-style-by-id.pipe.ts
+++ src/TrivyOperator.Dashboard/ClientApp/src/app/pipes/severity-dif-css-style-by-id.pipe.ts
@@ -1,19 +1,12 @@
 import { inject, Pipe, PipeTransform } from '@angular/core';
 import { SeverityUtils } from '../utils/severity.utils';
-import { DOCUMENT } from '@angular/common';
 
 @Pipe({
   name: 'severityDifCssStyleById',
   standalone: true,
 })
 export class SeverityDifCssStyleByIdPipe implements PipeTransform {
-  private document = inject(DOCUMENT);
-
   transform(severityId: number | string, severityCount: number = 0, showBorder: boolean = true): { [key: string]: string } {
-    const rootElement = this.document.documentElement;
-    const computedStyle = getComputedStyle(rootElement);
-    const contrastColor = computedStyle.getPropertyValue('--p-text-color');
-
     let cssColor = '';
     let opacity = '';
     let border = '';
@@ -21,7 +14,7 @@ export class SeverityDifCssStyleByIdPipe implements PipeTransform {
 
     cssColor = severityCount === 0 ? 'gray' : SeverityUtils.getCssColor(id);
     opacity = severityCount < 1 ? '0.4' : '1';
-    border = showBorder ? (severityCount !== 0 ? `2px solid ${contrastColor}` : '') : '';
+    border = showBorder ? (severityCount !== 0 ? `2px solid var(--p-text-color)` : '') : '';
 
     return {
       background: cssColor,
