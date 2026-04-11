d3f9797097a364b9aea49b3c8186de62fac4f839
raoulx24 <raoulx24@yahoo.com>
2026-04-11 19:51:07 +0300

    rehaul of vrhistory snapshot object
    not full vrcr in snapshot, but only vulnerabilities; cveshash is computed differently; new field in redis, vulnerabilitiesHash

--- src/TrivyOperator.Dashboard/Infrastructure/DistributedCache/DistributedCacheKeyExtensions.cs
+++ src/TrivyOperator.Dashboard/Infrastructure/DistributedCache/DistributedCacheKeyExtensions.cs
@@ -10,7 +10,7 @@ public static class DistributedCacheKeyExtensions
         => $"vr:{{{sie.Key.NamespaceName}}}:{sie.Key.Digest}";
 
     public static string GetKey(this Snapshot s)
-        => $"vr:{{{s.Key.NamespaceName}}}:{s.Key.Digest}:{s.Key.CvesHash}";
+        => s.Key.GetKey();
 
     public static string GetKey(this SnapshotKey sk) =>
         $"vr:{{{sk.NamespaceName}}}:{sk.Digest}:{sk.CvesHash}";
