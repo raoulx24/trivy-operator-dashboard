86129f797f00a3cda8bc85d3261307805ec97fda
raoulx24 <raoulx24@yahoo.com>
2026-05-31 21:04:52 +0300

    json system text - ReplacingSubstringConverter

---
+++ src/TrivyOperator.Dashboard/Domain/Utils/JsonConverters/ReplacingSubstringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TrivyOperator.Dashboard.Domain.Utils.JsonConverters;
+
+public class ReplacingSubstringConverter(string toSearchFor, string toReplaceWith, bool onlyRead = true) : JsonConverter<string>
+{
+    public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        string? s = reader.GetString();
+
+        return s?.Replace(toSearchFor, toReplaceWith) ??  string.Empty;
+    }
+
+    public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(!onlyRead ? value.Replace(toSearchFor, toReplaceWith) : value);
+    }
+}
